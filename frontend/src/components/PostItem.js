dd58b4403c84bd878ab94742f6babb1da0b20920
Dayan Petrov <dayanpetrov@Dayans-MacBook-Pro.local>
2018-04-06 20:38:20 +0300

    posts display with componens

--- frontend/src/components/PostItem.js
+++ frontend/src/components/PostItem.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 //import PropTypes from 'prop-types';
 //import { Link } from 'react-router-dom';
-//import { capitalize } from '../utils/helpers';
+import { capitalize } from '../utils/helpers';
 
 class PostItem extends Component {
   render() {
     const { post } = this.props
 
     return (
-        <div className="container">
-          {post && (
-            <p key={post.title}>{ post.title }</p>
-          )}
+        <div className="post-container">
+          <h3>{ post.title }</h3>
+          <p>
+            Posted by <span class="bold">{ post.author }</span> in <span class="bold">{ capitalize(post.category) }</span> on <span class="bold">{ post.timestamp }</span>
+          </p>
+          <p>{ post.body }</p>
+          <p>Vote score: { post.voteScore }</p>
+          <p>Comment count: { post.commentCount }</p>
         </div>
     );
   }
